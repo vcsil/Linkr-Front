821aec5ea6b8e4c7a290ddaf98584d6c01a7cf7e
luiz <lguilhermefl@gmail.com>
2022-08-09 18:54:52 -0300

    chorus: import global styles and react router to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./shared/styles/globalStyles.js";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 export default function App() {
-    return;
+    return (
+        <BrowserRouter>
+            <GlobalStyle />
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export const API_URL = process.env.REACT_APP_API_BASE_URL;
