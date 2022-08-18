f7bdd8f5b473f8c4171bf570a127dfe5aef2b436
luiz <lguilhermefl@gmail.com>
2022-08-18 19:54:46 -0300

    feat: show trending box in timeline

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import dotenv from "dotenv";
 import React from "react";
 
-import Hashtag from "./pages/Hashtag.js";
 import PageHashtag from "./pages/PageHashtag.js";
 import TelaSignInUp from "./pages/PageSignInUp/TelaSignInUp.js";
 import GlobalStyle from "./shared/styles/globalStyles.js";
@@ -24,9 +23,6 @@ export default function App() {
                 <Route path="/" element={<TelaSignInUp />} />
                 <Route path="/sign-up" element={<TelaSignInUp />} />
                 <Route path="/timeline" element={<Timeline />} />
-                {/* route created only for tests */}
-                <Route path="/hashtag" element={<Hashtag />} />
-                {/* fim route created only for tests */}
                 <Route path="/hashtag/:hashtag" element={<PageHashtag />} />
                 <Route path="*" element={<h1>Not Found</h1>} />
             </Routes>
